df77cbb274b4e5aca866558b9e8217f25f2733e6
passingthru67 <passingthru67@gmail.com>
2016-03-11 20:48:12 -0500

    Incorrect logic causing dock to animate out even when hovering.

--- workspaces-to-dock@passingthru67.gmail.com/dockedWorkspaces.js
+++ workspaces-to-dock@passingthru67.gmail.com/dockedWorkspaces.js
@@ -1632,7 +1632,7 @@ const DockedWorkspaces = new Lang.Class({
 
         if (this._settings.get_boolean('autohide')) {
             if (_DEBUG_) global.log("dockedWorkspaces: enableAutoHide - autohide settings true");
-            if (!(this._hoveringDash && !Main.overview.visible) || this.actor.hover) {
+            if (!this.actor.hover && !(this._hoveringDash && !Main.overview.visible)) {
                 if (_DEBUG_) global.log("dockedWorkspaces: enableAutoHide - mouse not hovering OR dock not using autohide, so animate out");
                 this._removeAnimations();
                 this._animateOut(this._settings.get_double('animation-time'), 0);
