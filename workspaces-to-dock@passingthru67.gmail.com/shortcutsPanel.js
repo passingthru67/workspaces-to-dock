6ee56dcab953c0eb6b88721026385dac4ef10209
passingthru67 <passingthru67@gmail.com>
2019-04-08 15:45:32 -0400

    Fix issue where prefs dialog not accessible from favorites panel popup menu.
    NOTE:
    This was not an issue until GS 3.30. Nevertheless the code needs
    to be updated to match GS 3.28 code.

--- workspaces-to-dock@passingthru67.gmail.com/shortcutsPanel.js
+++ workspaces-to-dock@passingthru67.gmail.com/shortcutsPanel.js
@@ -111,11 +111,14 @@ var ShortcutButtonMenu = new Lang.Class({
         this.actor.add_style_class_name('app-well-menu');
 
         // Chain our visibility and lifecycle to that of the source
-        source.actor.connect('notify::mapped', Lang.bind(this, function () {
+        this._sourcemappedId = source.actor.connect('notify::mapped', Lang.bind(this, function () {
             if (!source.actor.mapped)
                 this.close();
         }));
-        source.actor.connect('destroy', Lang.bind(this, function () { this.actor.destroy(); }));
+        source.actor.connect('destroy', Lang.bind(this, function () {
+            source.actor.disconnect(this._sourceMappedId);
+            this.destroy();
+        }));
 
         Main.uiGroup.add_actor(this.actor);
     },
